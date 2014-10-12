b3573c26e641f8c8d3084fa926948f07b14a6a3a
lxg <lxg1986@gmail.com>
2014-10-12 22:49:44 +0800

    add fs-extra,imageExif,findit node_modules

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("hello worl")
+#!/usr/bin/env node
+var finder = require('findit')(process.argv[2] || '.'); //查找文件/文件夹
+var ExifImage = require('exif').ExifImage;//获取图片的Exif信息
+var fs = require('fs-extra');//移动，复制，删除文件/文件夹
+var path = require('path');
+
+finder.on('directory', function (dir, stat, stop) {
+    var base = path.basename(dir);
+    if (base === '.git' || base === 'node_modules') stop()
+    else console.log(dir + '/')
+});
+
+finder.on('file', function (file, stat) {
+	try {
+		new ExifImage({ image : file }, function (error, exifData) {
+			if (error){
+				console.log('Error: '+error.message);
+			}else{
+				console.log(exifData.exif.DateTimeOriginal); // Do something with your data!
+			}
+		});
+	}catch (error) {
+		console.log('Error: ' + error.message);
+	}
+});
+

745fc4f87859b5448b7a49c3431a3a39ea20eb52
lxg <lxg1986@gmail.com>
2014-10-14 17:36:30 +0800

    classification photo by time

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 #!/usr/bin/env node
-var pathUrl = process.argv[2] || '.'
-var finder = require('findit')(pathUrl); //查找文件/文件夹
+var argvPath = process.argv[2] || '.' //传入的路径
+var rootFolder = "照片整理"
+var finder = require('findit')(argvPath); //查找文件/文件夹
 var ExifImage = require('exif').ExifImage;//获取图片的Exif信息
 var fs = require('fs-extra');//移动，复制，删除文件/文件夹
 var path = require('path');
 
-finder.on('directory', function (dir, stat, stop) {
-    var base = path.basename(dir);
-    if (base === '.git' || base === 'node_modules') stop()
-    else console.log(dir + '/')
-});
+function createRootFolder(){
+	//创建根目录文件夹
+	finder.on("directory",function(dir,stat,stop){
+		var base = path.basename(dir);
+		if (base === '.git' || base === 'node_modules') stop()
+		if(rootFolder !== dir){
+			fs.mkdirs(rootFolder, function(err){
+				if (err) return console.error(err);
+			});
+		}
+		
+		 if(rootFolder === dir){
+			stop()
+		}
+	})
+	finder.on('file', function (file, stat) {
+		try {
+			new ExifImage({ image : file }, function (error, exifData) {
+				if (error){
+					console.log('Error: '+error.message);
+				}else{
+					createPhotoFolder(exifData,file)
+				}
+			});
+		}catch (error) {
+			//console.log('Error: ' + error.message);
+		}
+	});
 
-finder.on('file', function (file, stat) {
-	try {
-		new ExifImage({ image : file }, function (error, exifData) {
-			if (error){
-				console.log('Error: '+error.message);
-			}else{
-				console.log(exifData.exif.DateTimeOriginal); // Do something with your data!
-			}
+}
+function createPhotoFolder(data,name){
+	var takenTime = data.exif.DateTimeOriginal
+	console.log(takenTime)
+	if(!takenTime){
+		return false
+	}	
+	var temp = takenTime.split(":")
+	takenTime = temp[0] + "-" + temp[1]
+	console.log(takenTime,"tanke")
+	fs.mkdirs("./" + rootFolder + "/" +takenTime, function(err){
+		if (err) return console.error(err);
+		var oldPath = argvPath + "/" + name
+		var newPath = "./" + rootFolder + "/" + takenTime + "/" + name
+		fs.move(oldPath,newPath, function(err){
+  			if (err) return console.error(err);
+  			console.log(name + " move success!")
 		});
-	}catch (error) {
-		console.log('Error: ' + error.message);
-	}
-});
+	});
+}
 
+
+createRootFolder()	
